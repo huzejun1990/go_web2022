e48b15bb77847ef0398c9657a6e54191cb228134
huzejun <huzejun1990>
2022-12-04 00:16:37 +0800

    lesson12 gin获取form参数 2022-12-4 00:16:24

---
+++ lesson12/main.go
@@ -0,0 +1,40 @@
+package main
+
+// 获取form表单提交的参数
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+	r.LoadHTMLFiles("./login.html", "./index.html")
+
+	r.GET("/login", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "login.html", nil)
+	})
+	// login post
+	r.POST("/login", func(c *gin.Context) {
+		// 获取form表单提交的数据
+		/*		username := c.PostForm("username")
+				password := c.PostForm("password")*/ //取到就返回值，取不到，就返回空字符串
+		//username := c.DefaultPostForm("username","somebody")
+		//password :=  c.DefaultPostForm("xxx","***")
+
+		username, ok := c.GetPostForm("username")
+		if !ok {
+			username = "is no"
+		}
+		password, ok := c.GetPostForm("password")
+		if !ok {
+			password = "***"
+		}
+
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"Name":     username,
+			"Password": password,
+		})
+	})
+
+	r.Run(":9090")
+}
